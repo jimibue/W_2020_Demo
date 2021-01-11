bc7c2f03401708582999f2c65999706c78cb87f9
dani7beth <danielledona28@gmail.com>
2021-01-11 12:00:17 -0700

    added dani component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import James from "./James";
+import Dani from "./Dani";
 
 function App() {
   return (
     <div className="App">
       <div>YOYOYO</div>
       <James />
+      <Dani />
     </div>
   );
 }
